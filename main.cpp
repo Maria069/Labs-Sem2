07e22e7915b194d5af4137add510edacba405233
Maria069 <klokova_maria06@mail.ru>
2025-03-09 23:01:27 +0300

    meow

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "mymath/mymath.h"
-
-int main(int, char**) {
-    std::cout << "НОД 10 и 22 = ";
-    std::cout << mymath::NOD(10, 22) << std::endl;
-
-    return 0;
-}
